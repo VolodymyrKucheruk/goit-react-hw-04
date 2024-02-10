7351599fc99a33a5c610c0d67f3247fe12971168
VolodymyrKucheruk <voloda.2014a@gmail.com>
2024-02-10 16:02:15 +0100

    end Homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,82 @@
 import css from "./App.module.css";
 import "modern-normalize";
-import { FcContacts } from "react-icons/fc";
-import { useState, useEffect } from "react";
-import { ContactList } from "./ContactList/ContactList";
-import { SearchBox } from "./SearchBox/SearchBox";
-import { ContactForm } from "./ContactForm/ContactForm";
+import { Toaster } from "react-hot-toast";
 
-const initialContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+import { useState, useEffect, useRef } from "react";
+import { fetchImages } from "./api";
+import { SearchBar } from "./SearchBar/SearchBar";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { ErrorMessage } from "./ErrorMessage/ErrorMessage";
+import { Loader } from "./Loader/Loader";
+import { LoadMoreBtn } from "./LoadMoreBtn/LoadMoreBtn";
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = JSON.parse(localStorage.getItem("saved-contacts"));
-    return savedContacts ? savedContacts.contacts : initialContacts;
-  });
+  const [imageElements, setImageElements] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [btnLoadMore, setBtnLoadMore] = useState(false);
+  const [error, setError] = useState(false);
+  const scrollRef = useRef(null);
 
-  const [filter, setFilter] = useState("");
+  const searchImageElements = async (newQuery) => {
+    setQuery(`${Date.now()}/${newQuery}`);
+    setPage(1);
+    setImageElements([]);
+  };
 
-  function addContact(newContact) {
-    setContacts((prevContacts) => [...prevContacts, newContact]);
-  }
-
-  function deleteContact(contactId) {
-    setContacts((prevContacts) =>
-      prevContacts.filter((contact) => contact.id !== contactId)
-    );
-  }
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const handleLoadMore = () => {
+    setPage((page) => page + 1);
+  };
 
   useEffect(() => {
-    localStorage.setItem("saved-contacts", JSON.stringify({ contacts }));
-  }, [contacts]);
+    if (query === "") {
+      return;
+    }
+
+    async function fetchData() {
+      try {
+        setLoading(true);
+        setError(false);
+        const fetchedData = await fetchImages(query.split("/")[1], page);
+        setBtnLoadMore(page === 1 && fetchedData.length === 0 ? false : true);
+        setImageElements((prevImageElements) => [
+          ...prevImageElements,
+          ...fetchedData,
+        ]);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [query, page]);
+
+  useEffect(() => {
+    if (page > 1) {
+      scrollRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        boundary: "viewport",
+      });
+    }
+  }, [page, imageElements]);
 
   return (
     <>
-      <div className={css.deviceHeader}>
-        <div className={css.deviceSensors}></div>
-      </div>
+      <SearchBar onSubmit={searchImageElements} />
+      {error && (
+        <ErrorMessage message=" Oops, there was an error, please try reloading 😭" />
+      )}
+      {imageElements.length > 0 && <ImageGallery items={imageElements} />}
+      {loading && <Loader />}
+      {imageElements.length > 0 && !loading && (
+        <LoadMoreBtn isLoad={handleLoadMore} />
+      )}
+      <div className={css.scroll} ref={scrollRef}></div>
 
-      <h1 className={css.title}>
-        Phonebook
-        <FcContacts className={css.bookitem} />
-      </h1>
-      <ContactForm value={filteredContacts} onAdd={addContact} />
-      <SearchBox value={filter} onChange={setFilter} />
-      <ContactList
-        value={filteredContacts}
-        onDelete={deleteContact}
-        onAdd={addContact}
-      />
+      <Toaster position="top-left" />
     </>
   );
 };
